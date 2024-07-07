ef7a70ed5806eac33ed098bbaaf4fc65741ff3f9
Fuzail Khan <96170691+Fuzaildev@users.noreply.github.com>
2024-07-07 16:14:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,151 @@
+// Back to Top Button
+
+(function ($) {
+    "use strict";
+
+    $(document).ready(function () {
+        "use strict";
+
+        // Navbar on scrolling
+        const button = document.querySelector('.button');
+        const menu = document.querySelector('.menu');
+
+        button.addEventListener('click', () => {
+            button.classList.toggle('-menu-open');
+            menu.classList.toggle('-open');
+            // button.classList.toggle('-menu-open');
+            // menu.classList.toggle('-open');
+
+        });
+
+
+        //Scroll back to top
+
+        var progressPath = document.querySelector('.progress-wrap path');
+        var pathLength = progressPath.getTotalLength();
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+        progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+        progressPath.style.strokeDashoffset = pathLength;
+        progressPath.getBoundingClientRect();
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+        var updateProgress = function () {
+            var scroll = $(window).scrollTop();
+            var height = $(document).height() - $(window).height();
+            var progress = pathLength - (scroll * pathLength / height);
+            progressPath.style.strokeDashoffset = progress;
+        }
+        updateProgress();
+        $(window).scroll(updateProgress);
+        var offset = 50;
+        var duration = 550;
+        jQuery(window).on('scroll', function () {
+            if (jQuery(this).scrollTop() > offset) {
+                jQuery('.progress-wrap').addClass('active-progress');
+            } else {
+                jQuery('.progress-wrap').removeClass('active-progress');
+            }
+        });
+        jQuery('.progress-wrap').on('click', function (event) {
+            event.preventDefault();
+            jQuery('html, body').animate({ scrollTop: 0 }, duration);
+            return false;
+        })
+
+
+    });
+
+})(jQuery);
+
+// Preloader
+
+$(document).ready(function () {
+    $(".preloader").fadeOut(5000);
+})
+
+//   Carousel Animation
+
+var nextBtn = document.querySelector('.next');
+prevBtn = document.querySelector('.prev'),
+    carousel = document.querySelector('.carousel'),
+    list = document.querySelector('.list'),
+    item = document.querySelectorAll('.item'),
+    runningTime = document.querySelector('.carousel .timeRunning')
+
+let timeRunning = 3000
+let timeAutoNext = 7000
+
+nextBtn.onclick = function () {
+    showSlider('next')
+}
+
+prevBtn.onclick = function () {
+    showSlider('prev')
+}
+
+let runTimeOut
+
+let runNextAuto = setTimeout(() => {
+    nextBtn.click()
+}, timeAutoNext)
+
+
+function resetTimeAnimation() {
+    runningTime.style.animation = 'none'
+    runningTime.offsetHeight /* trigger reflow */
+    runningTime.style.animation = null
+    runningTime.style.animation = 'runningTime 7s linear 1 forwards'
+}
+
+
+function showSlider(type) {
+    let sliderItemsDom = list.querySelectorAll('.carousel .list .item')
+    if (type === 'next') {
+        list.appendChild(sliderItemsDom[0])
+        carousel.classList.add('next')
+    } else {
+        list.prepend(sliderItemsDom[sliderItemsDom.length - 1])
+        carousel.classList.add('prev')
+    }
+
+    clearTimeout(runTimeOut)
+
+    runTimeOut = setTimeout(() => {
+        carousel.classList.remove('next')
+        carousel.classList.remove('prev')
+    }, timeRunning)
+
+
+    clearTimeout(runNextAuto)
+    runNextAuto = setTimeout(() => {
+        nextBtn.click()
+    }, timeAutoNext)
+
+    resetTimeAnimation() // Reset the running time animation
+}
+
+// Start the initial animation 
+resetTimeAnimation()
+
+
+// Marquee Animation 
+
+console.clear();
+
+gsap.registerPlugin(ScrollTrigger);
+
+let tl = gsap.timeline();
+let marquee = document.querySelector(".marquee");
+let content = document.querySelector(".marquee__content");
+let items = document.querySelectorAll(".marquee__item");
+let tlLength = items.length * 2;
+
+// animation timeline
+tl.to(".marquee__content", tlLength, {
+    xPercent: -100,
+    repeat: -1,
+    ease: "none"
+});
+
+let currentScale = 1;
+let scaleTl;
+
